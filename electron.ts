4065b80a2069795e774256869ef78014083a166c
xRaz <maxime.marquet1@gmail.com>
2017-11-03 14:15:13 +0100

    syntax

--- electron.ts
+++ electron.ts
@@ -1,6 +1,6 @@
 import electron = require('electron');
-// Module to control application life. (this variable should already exist)
-const app = electron.app
+// Module to control application life
+const app = electron.app;
 
 // This should be placed at top of main.js to handle setup events quickly
 if (handleSquirrelEvent(app)) {
@@ -39,14 +39,14 @@ function createWindow() {
 	}
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	mainWindow.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
+		mainWindow = null;
 	})
 }
 
@@ -60,7 +60,7 @@ app.on('window-all-closed', function() {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
 });
 
@@ -68,7 +68,7 @@ app.on('activate', function() {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
-		createWindow()
+		createWindow();
 	}
 });
 
